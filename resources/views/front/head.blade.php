4797a3b091e689c1c143fa5baa6fb0fe06d07351
Emanuellukas <lucasesn23@gmail.com>
2020-05-01 14:28:07 -0300

    ...

---
+++ resources/views/front/head.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{asset('front/assets/css/materialize.min.css')}}">
+    <link rel="stylesheet" href="{{asset('front/assets/css/style.css')}}">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800;900&display=swap"
+          rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossorigin="anonymous"></script>
+    <script src="{{asset('js/core/app.js')}}"></script>
+    <script src="{{asset('front/assets/js/materialize.min.js')}}"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Vidaloka&display=swap');
+    </style>
+    <title>Coopera Brasil</title>
+</head>
+<body>
