38ffb4ba1d4108f02e121b6e1d2654cdaffe21fb
Emanuellukas <lucasesn23@gmail.com>
2020-05-08 11:30:29 -0300

    ...

--- resources/views/front/contato.blade.php
+++ resources/views/front/contato.blade.php
@@ -3,3 +3,25 @@
     <contato></contato>
 </div>
 @include('front.footer')
+<script src="{{asset('js/app.js')}}"></script>
+<script src="{{asset('front/assets/js/materialize.min.js')}}"></script>
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v7.0&appId=358057404575901&autoLogAppEvents=1"></script>
+
+<script>
+    $('.dropdown-trigger').dropdown();
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+
+    function highlight_map_states() {
+        if ($(".states_section").length > 0) {
+            $(".states_section .list_states .item .link").hover(function () {
+                var a = "#state_" + $(this).text().toLowerCase();
+                $(a).attr("class", "state hover")
+            }, function () {
+                var a = "#state_" + $(this).text().toLowerCase();
+                $(a).attr("class", "state")
+            })
+        }
+    };
+</script>
