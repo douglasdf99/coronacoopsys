89c6d0b4fe6549a3e639645363d64a6a227af7e9
Emanuellukas <lucasesn23@gmail.com>
2020-05-08 11:23:27 -0300

    ....

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -19,22 +19,3 @@
         </div>
     </div>
 </footer>
-<script src="{{asset('js/app.js')}}"></script>
-<script src="{{asset('front/assets/js/materialize.min.js')}}"></script>
-<script>
-    $('.dropdown-trigger').dropdown();
-    $('.sidenav').sidenav();
-    $('.collapsible').collapsible();
-
-    function highlight_map_states() {
-        if ($(".states_section").length > 0) {
-            $(".states_section .list_states .item .link").hover(function () {
-                var a = "#state_" + $(this).text().toLowerCase();
-                $(a).attr("class", "state hover")
-            }, function () {
-                var a = "#state_" + $(this).text().toLowerCase();
-                $(a).attr("class", "state")
-            })
-        }
-    };
-</script>
